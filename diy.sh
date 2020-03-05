ea3de742c2ba5733ee595107dfac4cc96669bbb1
optdesigns <50645399+optdesigns@users.noreply.github.com>
2020-03-05 15:02:09 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,4 +6,10 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.123.99/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.3/g' package/base-files/files/bin/config_generate 
+git clone https://github.com/jefferymvp/luci-app-koolproxyR package/luci-app-koolproxyR 
+git clone https://github.com/rosywrt/luci-theme-rosy package/luci-theme-rosy 
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome 
+git clone https://github.com/vernesong/OpenClash package/OpenClash 
+./scripts/feeds update -a 
+./scripts/feeds install -a 
